4b0431e09ac3954b952885a8cf00ded7e2717a26
Jake Toomey <jtoom238@gmail.com>
2022-02-25 09:47:03 -0500

    initial commit/apollo init

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//apply apollo server with express app
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+//get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
